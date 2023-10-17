47f450c205d020757384aced54ed1dcbe99cd454
aishwaryavk445 <141556221+aishwaryavk445@users.noreply.github.com>
2023-10-17 20:49:02 +0530

    Create main.tf

---
+++ my-serverfiles/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server1" {
+  ami = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  key_name = "Awskeypair"
+  vpc_security_group_ids = ["sg-07fff356262072580"]
+  connection {
+     type         = "ssh"
+     user         = "ubuntu"
+     private_key  = file("./Awskeypair.pem")
+     host         = self.public_ip
+}
+provisioner "remote-exec" {
+    inline = ["echo 'wait to start the instance' "]
+}
+tags {
+  Name = "test-server1"
+}
+provisioner "local-exec" {
+    inline = " echo ${aws_instance.test-server1.public_ip} > inventory "
+}
+provisioner "remote-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/Banking-project1/my-serverfiles/finance-playbook.yml"
+}
+}
