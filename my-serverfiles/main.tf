573d5b6ad4721d91a88110962fb39d29b55bf1b7
aishwaryavk445 <141556221+aishwaryavk445@users.noreply.github.com>
2023-10-17 22:19:35 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -16,6 +16,6 @@ provisioner "local-exec" {
     command = " echo ${aws_instance.test-server.public_ip} > inventory "
 }
 provisioner "remote-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/Banking-project1/my-serverfiles/finance-playbook.yml"
+    command = " ansible-playbook /var/lib/jenkins/workspace/Banking-project1/my-serverfiles/finance-playbook.yml "
 }
 }
