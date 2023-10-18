3eab65464474fb6c3177d9b4abbc2e383ac43454
aishwaryavk445 <141556221+aishwaryavk445@users.noreply.github.com>
2023-10-18 15:57:00 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "test-server" {
   ami = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
-  key_name = "Awskeypair"
   vpc_security_group_ids = ["sg-0c1bd65465cdf390f"]
   connection {
      type         = "ssh"
@@ -12,9 +11,6 @@ resource "aws_instance" "test-server" {
 
 provisioner "remote-exec" {
     inline = ["echo 'wait to start the instance' "]
-}
-tags {
-  Name = "test-server"
 }
 provisioner "local-exec" {
     command = " echo ${aws_instance.test-server.public_ip} > inventory "
