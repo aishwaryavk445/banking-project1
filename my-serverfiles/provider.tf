9b60e983e955adba04c32780af6cf049d32510ab
aishwaryavk445 <141556221+aishwaryavk445@users.noreply.github.com>
2023-10-18 15:07:31 +0530

    Update provider.tf

--- my-serverfiles/provider.tf
+++ my-serverfiles/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 provider "aws" {
-region = "us-east-1"
+  region = var.region
 }
